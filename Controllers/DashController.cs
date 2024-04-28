d6c075274b761c56788288bda4f36ef09fe17f66
deya-123 <albettardeya@gmail.com>
2024-04-28 19:06:26 +0300

    commit

---
+++ Controllers/DashController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_one.Controllers
+{
+    public class DashController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Extensions/HtmlExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Task_one.Extensions
+{
+  
+    
+        public static class HtmlExtensions
+        {
+            public static bool ShouldShowElement(this IHtmlHelper htmlHelper, string controller, string action)
+            {
+                var currentController = htmlHelper.ViewContext.RouteData.Values["controller"]?.ToString();
+                var currentAction = htmlHelper.ViewContext.RouteData.Values["action"]?.ToString();
+                return string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase) &&
+                       string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    
+}
